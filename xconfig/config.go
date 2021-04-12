6fcf4cabaf6d663adf644eb877d1722a06840515
zusong <419894552@qq.com>
2021-04-12 23:41:00 +0800

    update:完善配置

--- xconfig/config.go
+++ xconfig/config.go
@@ -1,16 +1,35 @@
 package xconfig
 
 import (
+	"encoding/json"
+	"errors"
 	"io/ioutil"
+	"strings"
 
 	"gopkg.in/yaml.v2"
 )
 
 func Load(config interface{}, files ...string) (err error) {
-	data, err := ioutil.ReadFile(files[0])
+	for _, file := range files {
+		if err = LoadFile(config, file); err != nil {
+			return
+		}
+	}
+	return
+}
+
+func LoadFile(config interface{}, file string) (err error) {
+	data, err := ioutil.ReadFile(file)
 	if err != nil {
 		return err
 	}
-	err = yaml.Unmarshal(data, config)
-	return
+
+	switch {
+	case strings.HasSuffix(file, ".yaml") || strings.HasSuffix(file, ".yml"):
+		return yaml.Unmarshal(data, config)
+	case strings.HasSuffix(file, ".json"):
+		return json.Unmarshal(data, config)
+	default:
+		return errors.New("failed to decode configuration")
+	}
 }
